e3abc2dba160b60ce977aa2724b9ae00ce75c78d
twobanks <twobanks@me.com>
2021-01-08 12:48:56 -0300

    Nivel 3 - ReactJS - Mensagens de toast

--- nivel-3/gobarber-web/src/App.tsx
+++ nivel-3/gobarber-web/src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './context/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FC = () => {
 	return (
 		<>
-			<AuthProvider>
+			<AppProvider>
 				<SignIn />
-			</AuthProvider>
+			</AppProvider>
 
 			<GlobalStyle />
 		</>
